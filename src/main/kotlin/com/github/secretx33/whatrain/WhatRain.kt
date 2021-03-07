4cd22f4a9da438c1b8fdc6f4ddd9a6249ee73075
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2021-03-07 18:53:07 -0300

    Fixed severe issue with Koin
    I just discovered that if you run multiple plugins that use Koin together, and at least two of them use the Global Context, you're going to get errors, a lot of them... To prevent these situations, I've created a Custom Scope class and other re-implementations of Koin and they're all inside utils/Koin.kt, please make sure to import them instead of the ones of the default Koin lib, otherwise you're going to have the issue explained in the beggining.

--- src/main/kotlin/com/github/secretx33/whatrain/WhatRain.kt
+++ src/main/kotlin/com/github/secretx33/whatrain/WhatRain.kt
@@ -1,34 +1,43 @@
 package com.github.secretx33.whatrain
 
 import com.github.secretx33.whatrain.events.NoRainEvent
+import com.github.secretx33.whatrain.utils.*
 import com.github.secretx33.whatrain.utils.VersionUtils.serverVersion
 import com.github.secretx33.whatrain.utils.VersionUtils.v1_12_2_R01
-import com.github.secretx33.whatrain.utils.gameRule
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 import org.koin.core.component.KoinApiExtension
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
-import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 @KoinApiExtension
-class WhatRain : JavaPlugin(), KoinComponent {
+class WhatRain : JavaPlugin(), CustomKoinComponent {
+
+    // Module for Koin
+    // here you should put every single one of your dependencies that you want to be injected into other classes
+    val mod = module {
+        // Tells Koin to provide "this@WhatRain" object (our main class) if someone asks for "Plugin" or for "JavaPlugin"
+        // 'bind' adds a second type to the object, if we didn't have it here then Koin would only provide 'this@WhatRain' if
+        // someone asked specifically for a "Plugin" object
+        single<Plugin> { this@WhatRain } bind JavaPlugin::class
+        // single { NoRainEvent() } // example of telling koin to use NoRainEvent as dependency, them just use get<NoRainEvent>()
+        // in onEnable to make Koin instantiate it and you're done
+        // 'single' tells Koin this is singleton, it is instantiated only ONCE and provided to ALL consumers
+        // if you want to create a new object every time one asks for it, use 'factory' instead of 'single'
+    }
 
     override fun onEnable() {
         saveDefaultConfig()
-        // Our dependency injection library
+        // > Our dependency injection library <
+        // make SURE, and I repeat, SURE, that you're importing from utils/Koin.kt file and NOT from default Koin library
+        // otherwise you're going to get conflicts, a lot of them if someone else is also using the default Koin instance
         startKoin {
             // Makes Koin only print error messages
             printLogger(Level.ERROR)
-            modules(module {
-                // Tells Koin to provide "this@WhatRain" object (our main class) if someone asks for "Plugin" or for "JavaPlugin"
-                single<Plugin> { this@WhatRain } bind JavaPlugin::class
-            })
+            modules(mod)
         }
         if(config.getBoolean("enable-rain-cancel-eventlistener")) {
             // This "get" will tell Koin to inject our Plugin dependency here, get is NOT lazy, the instantiation and injection happens the moment you call "get" (opposed to "inject" which is lazy and will only create/inject the dependency when you use it for the first time)
@@ -51,5 +60,11 @@ class WhatRain : JavaPlugin(), KoinComponent {
         }
         if(config.getBoolean("display-loaded-msg"))
             server.consoleSender.sendMessage("${ChatColor.BLUE}[NoMoreRain]${ChatColor.GRAY} Loaded")
+    }
+
+    override fun onDisable() {
+        // Make sure to stop Koin is the last thing you do in onDisable function
+        unloadKoinModules(mod)
+        stopKoin()
     }
 }
